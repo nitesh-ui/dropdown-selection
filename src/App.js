2bd7a29ea1b3b958cffb999370fe83fc5d1b1324
Nitesh Pandey <112393577+nitesh-ui@users.noreply.github.com>
2022-12-16 13:52:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Dropdown from './components/Dropdown';
 import RxjsClass from './components/RxjsClass';
 import RxjsFunction from './components/RxjsFunction';
 import SelectValue from './components/SelectValue';
+import MultiSelectDropdown from './components/MultiSelectDropdown';
+
+const options = ["Nitesh", "Pappu", "Chaubey", "Amber", "Shubham", "Akshay", "Kritika"];
 
 function App() {
+  
+  const [multiOption, setMultiOption] = useState(["Akshay", "Kritika"]);
+  
   return (
     <div className="App">
       <Dropdown/>
       <SelectValue/>
       <RxjsClass/>
       <RxjsFunction/>
+      <MultiSelectDropdown defaultValue={options} setMultiOption={setMultiOption} optionList={options}/>
     </div>
   );
 }
